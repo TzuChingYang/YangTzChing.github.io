911e05d7003265f0d043fb6ea57e9cfee1e9aa95
YangTzChing <rsrs6543210@gmail.com>
2019-07-22 11:44:31 +0800

    First time test php of registration
    This time change
    1. index.heml-> action to php_test_1
    2.php_file-> Get info or not?

--- jackson/SQL/php_test_1.php
+++ jackson/SQL/php_test_1.php
@@ -1,24 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title> Registration info </title>
     <style>
 
     </style>
 </head>
 
 <body>
-<?php
-    $x=5 ;
-    $y=10 ;
 
-    function test(){
-        global $x,$y ;
-        $y=$x+$y ;
+<?php
+// define variables and set to empty values
+$usernameErr = $emailErr = $genderErr = $passwordErr = $birthdayErr = $colorErr = $agreementErr = "";
+$username = $email = $gender = $password = $birthday = $color = $agreement = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["username"])) {
+        $usernameErr = "Userbame is required";
+    } else {
+        $username = $_POST["username"];
     }
 
-    test();
-    echo $y ;
-    echo "test Mamp : x's value is->$x" ;
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = $_POST["email"];
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password is required";
+    } else {
+        $password = $_POST["password"];
+    }
+
+    if (empty($_POST["birthday"])) {
+        $birthdayErr = "Birthday is required";
+    } else {
+        $birthday = $_POST["birthday"];
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = $_POST["gender"];
+    }
+
+    if (empty($_POST["color"])) {
+        $colorErr = "Color is required";
+    } else {
+        $color = $_POST["color"];
+    }
+
+    if (empty($_POST["agreement"])) {
+        $agreementErr = "Agreement is required";
+    } else {
+        $agreement = $_POST["agreement"];
+    }
+}
 ?>
+
+
+<h2> Username: </h2><?php echo $username?> <br>
+<h2> Password: </h2><?php echo $password?> <br>
+<h2> Birthday: </h2><?php echo $birthday?> <br>
+<h2> Email: </h2><?php echo $email?> <br>
+<h2> Gender: </h2><?php echo $gender?> <br>
+<h2> Color: </h2><?php echo $color?> <br>
+<h2> Agreement: </h2><?php echo $agreement?> <br>
+
 </body>
 </html>
